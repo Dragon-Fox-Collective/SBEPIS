a323098b0cbfb027bc733b13cd26585a05ffdcb0
Jady Wright <iamjwright159@gmail.com>
2021-07-22 15:34:15 -0400

    Add a lil bit of captchaing

---
+++ Assets/Scripts/CaptchalogueCard.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptchalogueCard : MonoBehaviour
+{
+	public Transform cap0_0;
+	public Transform cap0_1;
+	public Transform cap0_2;
+	public Transform cap0_3;
+	public Transform cap0_4;
+	public Transform cap0_5;
+
+	public Transform cap1_0;
+	public Transform cap1_1;
+	public Transform cap1_2;
+	public Transform cap1_3;
+	public Transform cap1_4;
+	public Transform cap1_5;
+
+	public Transform cap2_0;
+	public Transform cap2_1;
+	public Transform cap2_2;
+	public Transform cap2_3;
+	public Transform cap2_4;
+	public Transform cap2_5;
+
+	public Transform cap3_0;
+	public Transform cap3_1;
+	public Transform cap3_2;
+	public Transform cap3_3;
+	public Transform cap3_4;
+	public Transform cap3_5;
+
+	public Transform cap4_0;
+	public Transform cap4_1;
+	public Transform cap4_2;
+	public Transform cap4_3;
+	public Transform cap4_4;
+	public Transform cap4_5;
+
+	public Transform cap5_0;
+	public Transform cap5_1;
+	public Transform cap5_2;
+	public Transform cap5_3;
+	public Transform cap5_4;
+	public Transform cap5_5;
+
+	public Transform cap6_0;
+	public Transform cap6_1;
+	public Transform cap6_2;
+	public Transform cap6_3;
+	public Transform cap6_4;
+	public Transform cap6_5;
+
+	public Transform cap7_0;
+	public Transform cap7_1;
+	public Transform cap7_2;
+	public Transform cap7_3;
+	public Transform cap7_4;
+	public Transform cap7_5;
+
+	private Transform[] holeCaps = new Transform[48];
+
+	private Item item;
+
+	private void Awake()
+	{
+		holeCaps[0] = cap0_0;
+		holeCaps[1] = cap0_1;
+		holeCaps[2] = cap0_2;
+		holeCaps[3] = cap0_3;
+		holeCaps[4] = cap0_4;
+		holeCaps[5] = cap0_5;
+
+		holeCaps[6] = cap1_0;
+		holeCaps[7] = cap1_1;
+		holeCaps[8] = cap1_2;
+		holeCaps[9] = cap1_3;
+		holeCaps[10] = cap1_4;
+		holeCaps[11] = cap1_5;
+
+		holeCaps[12] = cap2_0;
+		holeCaps[13] = cap2_1;
+		holeCaps[14] = cap2_2;
+		holeCaps[15] = cap2_3;
+		holeCaps[16] = cap2_4;
+		holeCaps[17] = cap2_5;
+
+		holeCaps[18] = cap3_0;
+		holeCaps[19] = cap3_1;
+		holeCaps[20] = cap3_2;
+		holeCaps[21] = cap3_3;
+		holeCaps[22] = cap3_4;
+		holeCaps[23] = cap3_5;
+
+		holeCaps[24] = cap4_0;
+		holeCaps[25] = cap4_1;
+		holeCaps[26] = cap4_2;
+		holeCaps[27] = cap4_3;
+		holeCaps[28] = cap4_4;
+		holeCaps[29] = cap4_5;
+
+		holeCaps[30] = cap5_0;
+		holeCaps[31] = cap5_1;
+		holeCaps[32] = cap5_2;
+		holeCaps[33] = cap5_3;
+		holeCaps[34] = cap5_4;
+		holeCaps[35] = cap5_5;
+
+		holeCaps[36] = cap6_0;
+		holeCaps[37] = cap6_1;
+		holeCaps[38] = cap6_2;
+		holeCaps[39] = cap6_3;
+		holeCaps[40] = cap6_4;
+		holeCaps[41] = cap6_5;
+
+		holeCaps[42] = cap7_0;
+		holeCaps[43] = cap7_1;
+		holeCaps[44] = cap7_2;
+		holeCaps[45] = cap7_3;
+		holeCaps[46] = cap7_4;
+		holeCaps[47] = cap7_5;
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Item collisionItem = collision.gameObject.GetComponent<Item>();
+		if (!item && collisionItem)
+		{
+			item = collisionItem;
+			item.transform.SetParent(transform);
+			item.gameObject.SetActive(false);
+			for (int i = 0; i < 48; i++)
+				if ((item.captchaHash & (1L << i)) != 0)
+					holeCaps[i].gameObject.SetActive(false);
+		}
+	}
+
+	private void OnMouseDown()
+	{
+		if (item)
+		{
+			item.transform.SetParent(null);
+			item.transform.position = transform.position + Vector3.up;
+			item.GetComponent<Rigidbody>().velocity = Vector3.up * 6;
+			item.gameObject.SetActive(true);
+			item = null;
+			foreach (Transform cap in holeCaps)
+				cap.gameObject.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public string captchaCode;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public long captchaHash { get; private set; }
+
+	private new Rigidbody rigidbody;
+
+	private void Awake()
+	{
+		captchaHash = hashCaptcha(captchaCode);
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void OnMouseDrag()
+	{
+		float distanceToScreen = Camera.main.WorldToScreenPoint(transform.position).z;
+		Vector3 newScreenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
+		Vector3 velocity = rigidbody.velocity;
+		transform.position = Vector3.SmoothDamp(transform.position, new Vector3(newScreenPosition.x, 2, newScreenPosition.z), ref velocity, 0.3f);
+		rigidbody.velocity = velocity;
+	}
+
+	private void OnMouseUp()
+	{
+		
+	}
+
+	private static readonly char[] hashCharacters =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'?', '!'
+	};
+
+	private static long hashCaptcha(string captchaCode)
+	{
+		if (captchaCode.Length != 8)
+			throw new ArgumentException("Captcha code must have 8 characters", nameof(captchaCode));
+
+		long hash = 0;
+		for (int i = 0; i < captchaCode.Length; i++)
+		{
+			if (Array.IndexOf(hashCharacters, captchaCode[i]) == -1)
+				throw new ArgumentException("Captcha code contains illegal characters", nameof(captchaCode));
+
+			hash |= (1L << i * 6) * Array.IndexOf(hashCharacters, captchaCode[i]);
+		}
+		return hash;
+	}
 }
