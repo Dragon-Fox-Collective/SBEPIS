ea6469394f16b3da2cad63eed1f3bff8d49a0562
Jady Wright <iamjwright159@gmail.com>
2022-02-21 18:36:14 -0500

    Make buttons work for all rotational directions

--- Assets/Interaction/PhysicsButton.cs
+++ Assets/Interaction/PhysicsButton.cs
@@ -19,10 +19,17 @@ namespace SBEPIS.Interaction
 		public new Rigidbody rigidbody { get; private set; }
 		public ConfigurableJoint joint { get; private set; }
 
+		private Vector3 initialLocalRotation;
+
 		private void Awake()
 		{
 			rigidbody = GetComponent<Rigidbody>();
 			joint = GetComponent<ConfigurableJoint>();
+		}
+
+		private void Start()
+		{
+			initialLocalRotation = transform.localRotation.eulerAngles;
 		}
 
 		private void Update()
@@ -53,32 +60,106 @@ namespace SBEPIS.Interaction
 				case ButtonAxis.YPosition:
 				case ButtonAxis.ZPosition:
 					return GetLinearDirectionValue(transform.localPosition).Map(GetLinearDirectionValue(GetWorldLinearStartPoint()), GetLinearDirectionValue(GetWorldLinearEndPoint()), 0, 1);
+
+				case ButtonAxis.XRotation:
+				case ButtonAxis.YRotation:
+				case ButtonAxis.ZRotation:
+					return GetRotationalDirectionValue(transform.localRotation.eulerAngles - initialLocalRotation).Map(GetRotationalDirectionValue(GetLocalRotationalStartPoint()), GetRotationalDirectionValue(GetLocalRotationalEndPoint()), 0, 1);
+
+				default:
+					return 0;
 			}
-			return 0;
 		}
 
 		private Vector3 GetWorldLinearStartPoint()
 		{
-			return transform.TransformPoint(transform.InverseTransformPoint(joint.connectedAnchor) - joint.linearLimit.limit * GetLinearAxis());
+			return transform.TransformPoint(transform.InverseTransformPoint(joint.connectedAnchor) - joint.linearLimit.limit * GetAxis());
 		}
 
 		private Vector3 GetWorldLinearEndPoint()
 		{
-			return transform.TransformPoint(transform.InverseTransformPoint(joint.connectedAnchor) + joint.linearLimit.limit * GetLinearAxis());
+			return transform.TransformPoint(transform.InverseTransformPoint(joint.connectedAnchor) + joint.linearLimit.limit * GetAxis());
 		}
 
-		private Vector3 GetLinearAxis()
+		private Vector3 GetLocalRotationalStartPoint()
+		{
+			return GetLowRotationalLimit() * GetAxis();
+		}
+
+		private Vector3 GetLocalRotationalEndPoint()
+		{
+			return GetHighRotationalLimit() * GetAxis();
+		}
+
+		private float GetLowRotationalLimit()
+		{
+			switch (axis)
+			{
+				case ButtonAxis.XRotation:
+					return joint.lowAngularXLimit.limit;
+
+				case ButtonAxis.YRotation:
+					return -joint.angularYLimit.limit;
+
+				case ButtonAxis.ZRotation:
+					return -joint.angularZLimit.limit;
+
+				default:
+					return 0;
+			}
+		}
+
+		private float GetHighRotationalLimit()
+		{
+			switch (axis)
+			{
+				case ButtonAxis.XRotation:
+					return joint.highAngularXLimit.limit;
+
+				case ButtonAxis.YRotation:
+					return joint.angularYLimit.limit;
+
+				case ButtonAxis.ZRotation:
+					return joint.angularZLimit.limit;
+
+				default:
+					return 0;
+			}
+		}
+
+		private Vector3 GetAxis()
 		{
 			switch (axis)
 			{
 				case ButtonAxis.XPosition:
+				case ButtonAxis.XRotation:
 					return Vector3.right;
 
 				case ButtonAxis.YPosition:
+				case ButtonAxis.YRotation:
 					return Vector3.up;
 
 				case ButtonAxis.ZPosition:
+				case ButtonAxis.ZRotation:
 					return Vector3.forward;
+
+				default:
+					return Vector3.zero;
+			}
+		}
+
+		private Vector3 GetRotationalAxis(Transform transform)
+		{
+			switch (axis)
+			{
+				case ButtonAxis.XRotation:
+					return transform.right;
+
+				case ButtonAxis.YRotation:
+					return transform.up;
+
+				case ButtonAxis.ZRotation:
+					return transform.forward;
 
 				default:
 					return Vector3.zero;
@@ -97,6 +178,24 @@ namespace SBEPIS.Interaction
 
 				case ButtonAxis.ZPosition:
 					return vector.z;
+
+				default:
+					return 0;
+			}
+		}
+
+		private float GetRotationalDirectionValue(Vector3 eulers)
+		{
+			switch (axis)
+			{
+				case ButtonAxis.XRotation:
+					return eulers.x;
+
+				case ButtonAxis.YRotation:
+					return eulers.y;
+
+				case ButtonAxis.ZRotation:
+					return eulers.z;
 
 				default:
 					return 0;
