88b305ccb37aadd0cd864d076cceb32760ba16b1
Jady Wright <iamjwright159@gmail.com>
2022-03-02 23:07:55 -0500

    Add discord rich presence

---
+++ Assets/Plugins/DiscordGameSDK/DiscordController.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Discord
+{
+	public class DiscordController : MonoBehaviour
+	{
+		private static readonly int startEpoch = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+
+		private Discord discord = new Discord(948761142950514738, (UInt64)CreateFlags.NoRequireDiscord);
+
+		private void Start()
+		{
+			InvokeRepeating(nameof(UpdatePresence), 0, 60);
+		}
+
+		private void Update()
+		{
+			discord.RunCallbacks();
+		}
+
+		private void OnDestroy()
+		{
+			discord.GetActivityManager().ClearActivity(result => print(result));
+		}
+
+		private void UpdatePresence()
+		{
+			print("Updating presence");
+			discord.GetActivityManager().UpdateActivity(new Activity
+			{
+				Details = "Exploring " + SceneManager.GetActiveScene().name,
+				State = "https://discord.gg/qHREQu7Zxm",
+				Timestamps =
+				{
+					Start = startEpoch
+				},
+				Assets =
+				{
+					LargeImage = "sbepis",
+					LargeText = "SBEPIS",
+					SmallImage = "bitflower",
+					SmallText = "sbepis",
+				},
+				Instance = true,
+			}, result => print(result));
+		}
+	}
+}
