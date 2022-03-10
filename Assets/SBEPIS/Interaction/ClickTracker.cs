99f093a6a4aa56352a76f385fc155d3868bc7683
Jady Wright <iamjwright159@gmail.com>
2022-03-10 10:53:06 -0500

    Make main menu use clicktrackers

--- Assets/SBEPIS/Interaction/ClickTracker.cs
+++ Assets/SBEPIS/Interaction/ClickTracker.cs
@@ -4,16 +4,38 @@ using UnityEngine.Events;
 
 namespace SBEPIS.Interaction
 {
+	[RequireComponent(typeof(Grabbable))]
 	public class ClickTracker : MonoBehaviour
 	{
-		[Tooltip("Must be greater than your tap time or it won't work")]
-		public float tapTime = 0.5f;
-		public UnityEvent OnClick;
+		public UnityEvent onClick;
 
+		/// <summary>
+		/// Must be greater than your mouse tap time or it won't work
+		/// </summary>
+		private const float TAP_TIME = 0.3f;
+
+		private Grabbable grabbable;
 		private bool isTracking;
 		private float startTime;
 
-		public void StartTracking()
+		private void Awake()
+		{
+			grabbable = GetComponent<Grabbable>();
+		}
+
+		private void OnEnable()
+		{
+			grabbable.onGrab.AddListener(StartTracking);
+			grabbable.onDrop.AddListener(FinishTracking);
+		}
+
+		private void OnDisable()
+		{
+			grabbable.onGrab.RemoveListener(StartTracking);
+			grabbable.onDrop.RemoveListener(FinishTracking);
+		}
+
+		private void StartTracking(Grabber grabber)
 		{
 			if (isTracking)
 				throw new InvalidOperationException($"Click tracker {gameObject} is already tracking");
@@ -21,14 +43,14 @@ namespace SBEPIS.Interaction
 			startTime = Time.unscaledTime;
 		}
 
-		public void FinishTracking()
+		private void FinishTracking(Grabber grabber)
 		{
 			if (!isTracking)
 				throw new InvalidOperationException($"Click tracker {gameObject} is not tracking yet");
 			isTracking = false;
-			print($"{gameObject} finished clicking, held for {Time.unscaledTime - startTime}, clicked? {Time.unscaledTime - startTime <= tapTime}");
-			if (Time.unscaledTime - startTime <= tapTime)
-				OnClick.Invoke();
+			print($"{gameObject} finished clicking, held for {Time.unscaledTime - startTime}, clicked? {Time.unscaledTime - startTime <= TAP_TIME}");
+			if (Time.unscaledTime - startTime <= TAP_TIME)
+				onClick.Invoke();
 		}
 	}
 }
