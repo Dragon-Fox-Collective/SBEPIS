4cf9026c3956cd3fc893d6cb28ffe9a20c6e61fe
Jady Wright <iamjwright159@gmail.com>
2022-07-17 19:34:46 -0400

    Erase the  unused newer strength settings

--- Assets/SBEPIS/Interaction/StrengthSettings.cs
+++ Assets/SBEPIS/Interaction/StrengthSettings.cs
@@ -7,13 +7,6 @@ namespace SBEPIS.Interaction
 	[CreateAssetMenu]
 	public class StrengthSettings : ScriptableObject
 	{
-		[Header("New system")]
-		public float velocityTarget = 120;
-		public float maxAcceleration = 20;
-		public float angularVelocityTarget = 1;
-		public float maxAngularAcceleration = 10;
-
-		[Header("Old system")]
 		public float strength = 10;
 		public float maxEffectiveDistance = 1;
 		public float torque = 10;
