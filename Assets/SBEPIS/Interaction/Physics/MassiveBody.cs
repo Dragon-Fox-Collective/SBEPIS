3657f8bf82f218f90437fc18773493055c0ba06c
Jady Wright <iamjwright159@gmail.com>
2022-07-20 22:59:50 -0400

    Replace massivefield with massivepoint

--- Assets/SBEPIS/Interaction/Physics/MassiveBody.cs
+++ Assets/SBEPIS/Interaction/Physics/MassiveBody.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 namespace SBEPIS.Interaction.Physics
 {
-	public abstract class MassiveBody : MonoBehaviour
+	public class MassiveBody : MonoBehaviour
 	{
+		public float gravity = 9.81f;
+		public float falloff = 2;
 		public int priority = 0;
 
-		public abstract Vector3 GetGravity(GravitySum gravityNormalizer);
+		public bool useX = true;
+		public bool useY = true;
+		public bool useZ = true;
+
+		public Vector3 GetGravity(GravitySum gravityNormalizer)
+		{
+			Vector3 localDelta = -transform.InverseTransformPoint(gravityNormalizer.rigidbody.worldCenterOfMass);
+			Vector3 toggledLocalDelta = new(useX ? localDelta.x : 0, useY ? localDelta.y : 0, useZ ? localDelta.z : 0);
+			Vector3 globalDelta = transform.TransformDirection(toggledLocalDelta);
+			return gravity / Mathf.Pow(globalDelta.magnitude, falloff) * globalDelta.normalized;
+		}
 
 		private void OnTriggerEnter(Collider other)
 		{
