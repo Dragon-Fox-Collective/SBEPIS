3a90a41c951f020fdc42aed1fa32bcd128f941cb
Jady Wright <iamjwright159@gmail.com>
2022-07-21 23:40:03 -0400

    (Hopefully) Stabilize rigidbody orientation snapping a little bit

--- Assets/SBEPIS/Utils/TransformSync.cs
+++ Assets/SBEPIS/Utils/TransformSync.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SBEPIS.Utils
 {
 	public class TransformSync : MonoBehaviour
 	{
+		private new Rigidbody rigidbody;
+
+		private void Awake()
+		{
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
 		public void SyncPosition(Transform other)
 		{
-			transform.position = other.position;
+			if (rigidbody)
+				rigidbody.MovePosition(other.position);
+			else
+				transform.position = other.position;
 		}
 
 		public void SyncRotation(Transform other)
 		{
-			transform.rotation = other.rotation;
+			if (rigidbody)
+				rigidbody.MoveRotation(other.rotation);
+			else
+				transform.rotation = other.rotation;
 		}
 	}
 }
