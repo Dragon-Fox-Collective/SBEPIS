a3db094fa881c544f4a6659d8a5c074877d6e9aa
Jady Wright <iamjwright159@gmail.com>
2022-07-22 20:09:53 -0400

    Add hand zooming for flatscreen

--- Assets/SBEPIS/Interaction/Controller/Flatscreen/FlatscreenHandTrackerPositioner.cs
+++ Assets/SBEPIS/Interaction/Controller/Flatscreen/FlatscreenHandTrackerPositioner.cs
@@ -12,19 +12,28 @@ namespace SBEPIS.Interaction.Controller.Flatscreen
 		public Grabber leftGrabber;
 		public LayerMask raycastMask = 1;
 		public float raycastDistance = 2;
+		public float minimumZoomDistance = 1;
+		public float zoomSensitivity = 0.1f;
 
 		public Transform rightHoldPosition;
 		public Transform leftHoldPosition;
 
 		public Orientation playerOrientation;
 
-		private bool zoomed;
+		private float zoomAmount;
+
+		private void Awake()
+		{
+			zoomAmount = minimumZoomDistance;
+		}
 
 		private void FixedUpdate()
 		{
-			if (!rightGrabber.heldCollider && CastHand(out RaycastHit hit, rightGrabber))
+			if (rightGrabber.heldCollider)
+				rightTracker.transform.SetPositionAndRotation(transform.position + transform.forward * zoomAmount, transform.rotation);
+			else if (CastHand(out RaycastHit hit, rightGrabber))
 				rightTracker.transform.SetPositionAndRotation(hit.point, Quaternion.LookRotation(-hit.normal, playerOrientation.upDirection));
-			else if (rightGrabber.heldCollider || CastShortRangeGrab(out _, rightGrabber))
+			else if (CastShortRangeGrab(out _, rightGrabber))
 				rightTracker.transform.SetPositionAndRotation(transform.position + transform.forward * (raycastDistance - rightGrabber.shortRangeGrabDistace), transform.rotation);
 			else
 				rightTracker.transform.SetPositionAndRotation(rightHoldPosition.position, rightHoldPosition.rotation);
@@ -34,10 +43,7 @@ namespace SBEPIS.Interaction.Controller.Flatscreen
 
 		public void OnZoom(CallbackContext context)
 		{
-			if (!rightGrabber.heldCollider || context.ReadValue<float>() > 0)
-				zoomed = false;
-			else if (context.ReadValue<float>() < 0)
-				zoomed = true;
+			zoomAmount = Mathf.Clamp(zoomAmount + context.ReadValue<float>() * zoomSensitivity, minimumZoomDistance, raycastDistance);
 		}
 
 		private bool CastHand(out RaycastHit hit, Grabber grabber)
