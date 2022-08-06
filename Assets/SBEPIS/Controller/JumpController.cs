caa4d46d6b6d37aea330fe08134413915b247b93
Jady Wright <iamjwright159@gmail.com>
2022-08-06 18:34:56 -0400

    Add physical jumping

--- Assets/SBEPIS/Controller/JumpController.cs
+++ Assets/SBEPIS/Controller/JumpController.cs
@@ -3,13 +3,14 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 namespace SBEPIS.Controller
 {
-	[RequireComponent(typeof(Rigidbody), typeof(Orientation))]
+	[RequireComponent(typeof(Orientation))]
 	public class JumpController : MonoBehaviour
 	{
+		public ConfigurableJoint footballJoint;
 		public float jumpSpeed = 3;
 		public float groundDetectorDelay = 0.5f;
+		public Vector3 connectedAnchorTarget;
 
-		private new Rigidbody rigidbody;
 		private Orientation groundDetector;
 
 		public const float KEY_BUFFER = 0.1f;
@@ -17,23 +18,25 @@ namespace SBEPIS.Controller
 
 		private float jumpBuffer;
 		private float coyoteTime;
-		private Rigidbody lastGround;
+
+		private Vector3 initialConnectedAnchor;
+		private bool isJumping;
 
 		private void Awake()
 		{
-			rigidbody = GetComponent<Rigidbody>();
 			groundDetector = GetComponent<Orientation>();
+			initialConnectedAnchor = footballJoint.connectedAnchor;
 		}
 
 		private void FixedUpdate()
 		{
 			coyoteTime = groundDetector.isGrounded ? COYOTE_TIME : coyoteTime - Time.fixedDeltaTime;
 
-			if (groundDetector.isGrounded)
-				lastGround = groundDetector.groundRigidbody;
-
 			if (jumpBuffer > 0)
 				jumpBuffer = Jump() ? 0 : jumpBuffer - Time.fixedDeltaTime;
+
+			if (isJumping)
+				MoveFoot();
 		}
 
 		private bool Jump()
@@ -41,12 +44,23 @@ namespace SBEPIS.Controller
 			if (coyoteTime <= 0 || (!groundDetector.isFalling && groundDetector.verticalVelocity.magnitude >= jumpSpeed))
 				return false;
 
-			MovementController.AddVelocityAgainstGround(rigidbody, groundDetector.upDirection * jumpSpeed - groundDetector.verticalVelocity, lastGround);
+			isJumping = true;
 
 			groundDetector.Delay(groundDetectorDelay);
 			coyoteTime = 0;
 
 			return true;
+		}
+
+		private void MoveFoot()
+		{
+			if (footballJoint.connectedAnchor == connectedAnchorTarget)
+			{
+				isJumping = false;
+				footballJoint.connectedAnchor = initialConnectedAnchor;
+			}
+			else
+				footballJoint.connectedAnchor = Vector3.MoveTowards(footballJoint.connectedAnchor, connectedAnchorTarget, jumpSpeed * Time.fixedDeltaTime);
 		}
 
 		public void OnJump(CallbackContext context)
