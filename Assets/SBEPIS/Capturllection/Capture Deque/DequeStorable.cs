904584d58fe4343d4adf2024353739e2939e3357
Jady Wright <iamjwright159@gmail.com>
2022-08-13 16:04:26 -0400

    Rearrange storePredicates

--- Assets/SBEPIS/Capturllection/Capture Deque/DequeStorable.cs
+++ Assets/SBEPIS/Capturllection/Capture Deque/DequeStorable.cs
@@ -1,4 +1,5 @@
 using SBEPIS.Controller;
+using SBEPIS.Thaumaturgy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,25 @@ namespace SBEPIS.Capturllection
 	public class DequeStorable : MonoBehaviour
 	{
 		public bool isStoringAllowed = true;
+		public List<Func<bool>> storePredicates = new();
 
 		public bool isStored { get; set; }
 		public Grabbable grabbable { get; private set; }
-		public readonly List<Func<bool>> storePredicates = new();
 		public bool canStore => storePredicates.All(predicate => predicate.Invoke());
 
 		private void Awake()
 		{
 			grabbable = GetComponent<Grabbable>();
 			storePredicates.Add(() => isStoringAllowed);
+
+			// This sucks but it's the best place to put it for now :/
+			Capturllectainer container = GetComponent<Capturllectainer>();
+			if (container)
+				storePredicates.Add(() => container.capturedItem);
+
+			Punchable punchable = GetComponent<Punchable>();
+			if (punchable)
+				storePredicates.Add(() => punchable.punchedBits == Bits.BitSet.NOTHING);
 		}
 	}
 }
