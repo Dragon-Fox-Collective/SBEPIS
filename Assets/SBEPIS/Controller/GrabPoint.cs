0c3decf2bea4aef4328535ca48f6b896a6bfdb56
Jady Wright <iamjwright159@gmail.com>
2022-08-16 11:31:39 -0400

    Add grab points

---
+++ Assets/SBEPIS/Controller/GrabPoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SBEPIS.Controller
+{
+	public class GrabPoint : MonoBehaviour
+	{
+		public Collider colliderToGrab;
+	}
+}
