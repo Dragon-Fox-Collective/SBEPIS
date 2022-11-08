a54d9b203c8f332763804f7ded2b677700543b8a
Jady Wright <iamjwright159@gmail.com>
2022-11-08 14:25:56 -0500

    Add testing for Suicide Burn algorithms

--- Assets/SBEPIS/Controller/SuicideBurn.cs
+++ Assets/SBEPIS/Controller/SuicideBurn.cs
@@ -1,19 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SBEPIS.Controller
 {
 	[RequireComponent(typeof(Rigidbody))]
-	public class OrienterJoint : MonoBehaviour
+	public class SuicideBurn : MonoBehaviour
 	{
 		public float acceleration = 1;
 
 
-		const float VELOCITY_GRAPH_MAX = 6.0f;
+		const float VELOCITY_GRAPH_MAX = 10.0f;
 
 		[DebugGUIPrint, DebugGUIGraph(group: 0, min: -VELOCITY_GRAPH_MAX, max: VELOCITY_GRAPH_MAX, autoScale: false, r: 1.0f, g: 0.3f, b: 0.3f)]
 		private float currentVelocity;
 
-		const float DISTANCE_GRAPH_MAX = 3.0f;
+		const float DISTANCE_GRAPH_MAX = 10.0f;
 
 		[DebugGUIPrint, DebugGUIGraph(group: 1, min: -DISTANCE_GRAPH_MAX, max: DISTANCE_GRAPH_MAX, autoScale: false, r: 1.0f, g: 0.3f, b: 0.3f)]
 		private float currentDistance;
@@ -24,7 +26,7 @@ namespace SBEPIS.Controller
 		[DebugGUIPrint, DebugGUIGraph(group: 1, min: -1.5f, max: 1.5f, autoScale: false, r: 1.0f, g: 1.0f, b: 1.0f)]
 		private float accelerationSign;
 
-		const float TIME_GRAPH_MAX = 2.0f;
+		const float TIME_GRAPH_MAX = 10.0f;
 
 		[DebugGUIPrint, DebugGUIGraph(group: 2, min: -TIME_GRAPH_MAX, max: TIME_GRAPH_MAX, autoScale: false, r: 0.3f, g: 1.0f, b: 0.3f)]
 		private float timeToMax;
@@ -33,36 +35,27 @@ namespace SBEPIS.Controller
 		private float velocitySign;
 
 
-		private Vector3 up = Vector3.up;
-		private bool isStanding = false;
+		public Transform target;
+		public float initialVelocity;
+
 		private new Rigidbody rigidbody;
 
 		private void Awake()
 		{
 			rigidbody = GetComponent<Rigidbody>();
+			rigidbody.velocity = (transform.position - target.position).normalized * initialVelocity;
 
 			DebugGUI.Log("Bepis");
-		}
-		
-		public void Orient(Vector3 up)
-		{
-			isStanding = up != Vector3.zero;
-			if (isStanding)
-				this.up = up.normalized;
 		}
 
 		private void FixedUpdate()
 		{
-			if (!isStanding)
-				return;
+			Vector3 delta = target.position - transform.position;
 
-			Quaternion delta = Quaternion.FromToRotation(transform.up, up);
-			if (delta.w < 0) delta = delta.Select(x => -x);
-			delta.ToAngleAxis(out currentDistance, out Vector3 axis);
-			currentDistance *= Mathf.Deg2Rad;
+			currentDistance = delta.magnitude;
 
-			velocitySign = Vector3.Dot(rigidbody.angularVelocity, axis) > 0 ? -1 : 1;
-			currentVelocity = rigidbody.angularVelocity.magnitude * velocitySign;
+			velocitySign = Vector3.Dot(rigidbody.velocity, delta) > 0 ? -1 : 1;
+			currentVelocity = rigidbody.velocity.magnitude * velocitySign;
 
 			timeToMax = currentVelocity / acceleration;
 			maxPoint = currentDistance - 0.5f * currentVelocity * currentVelocity / -acceleration;
@@ -72,10 +65,10 @@ namespace SBEPIS.Controller
 			accelerationSign = decelerate ? 1 : -1;
 
 			float deltaVelocity = accelerationSign * acceleration * Time.fixedDeltaTime;
-			rigidbody.angularVelocity += deltaVelocity * -axis;
+			rigidbody.velocity += deltaVelocity * -delta.normalized;
 
-			if (currentDistance < rigidbody.angularVelocity.magnitude * Time.fixedDeltaTime && rigidbody.angularVelocity.magnitude < 2 * acceleration * Time.fixedDeltaTime)
-				rigidbody.angularVelocity = currentDistance * axis / Time.fixedDeltaTime;
+			if (currentDistance < rigidbody.velocity.magnitude * Time.fixedDeltaTime && rigidbody.velocity.magnitude < 2 * acceleration * Time.fixedDeltaTime)
+				rigidbody.velocity = delta / Time.fixedDeltaTime;
 		}
 	}
 }
