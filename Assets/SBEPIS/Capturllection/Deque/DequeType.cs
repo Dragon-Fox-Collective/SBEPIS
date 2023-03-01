4b738c3411faa9304b3d0c09d0c69a8d788ea4f3
Jady Wright <iamjwright159@gmail.com>
2023-03-01 13:50:02 -0500

    Add functions for storing items using existing cards

--- Assets/SBEPIS/Capturllection/Deque/DequeType.cs
+++ Assets/SBEPIS/Capturllection/Deque/DequeType.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,21 @@ namespace SBEPIS.Capturllection
 		public abstract void LayoutTargets(List<DequeStorable> cards, Dictionary<DequeStorable, CardTarget> targets);
 		
 		public abstract bool CanFetch(List<DequeStorable> cards, DequeStorable card);
-		public abstract int GetIndexToInsertInto(List<DequeStorable> cards, DequeStorable card);
+		public abstract int GetIndexToStoreInto(List<DequeStorable> cards);
+		public abstract int GetIndexToInsertCardBetween(List<DequeStorable> cards, DequeStorable card);
+
+		public static bool HasEmptyContainer(DequeStorable card) => card.TryGetComponent(out Capturellectainer container) && !container.hasCapturedItem;
+		public static bool DoesntHaveEmptyContainer(DequeStorable card) => !HasEmptyContainer(card);
+		
+		public static int OrBeforeAllCards(List<DequeStorable> cards) => 0;
+		public static int OrAfterAllCards(List<DequeStorable> cards) => cards.Count;
+		public static int OrFirstCard(List<DequeStorable> cards) => 0;
+		public static int OrLastCard(List<DequeStorable> cards) => cards.Count - 1;
+		
+		public static int GetFirstIndexWhere(List<DequeStorable> cards, Predicate<DequeStorable> predicate, Func<List<DequeStorable>, int> defaultIndexFunc)
+		{
+			int index = cards.FindIndex(predicate);
+			return index != -1 ? index : defaultIndexFunc.Invoke(cards);
+		}
 	}
 }
