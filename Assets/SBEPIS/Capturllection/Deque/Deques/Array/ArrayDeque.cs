89a2141eb274a94567ba591d04c47f25bdba9159
Jady Wright <iamjwright159@gmail.com>
2023-03-08 20:18:05 -0500

    Add offset toggles for queue, deque, and array, and fix directions for cyclones

--- Assets/SBEPIS/Capturllection/Deque/Deques/Array/ArrayDeque.cs
+++ Assets/SBEPIS/Capturllection/Deque/Deques/Array/ArrayDeque.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SBEPIS.Capturllection.Deques
 {
 	public class ArrayDeque : DequeBase<ArrayState>
 	{
-		public float overlap = 0.05f;
+		public bool offsetFromEnd = false;
+		[FormerlySerializedAs("overlap")]
+		public float offset = 0.05f;
 		public float wobbleAmplitude = 0.1f;
 		public float wobbleTimeFactor = 1;
 		public float wobbleSpaceFactor = 1;
@@ -17,7 +20,9 @@ namespace SBEPIS.Capturllection.Deques
 			
 			List<Vector3> sizes = inventory.Select(storable => storable.maxPossibleSize).ToList();
 			Vector3 absDirection = direction.Select(Mathf.Abs);
-			float lengthSum = -overlap * (inventory.Count - 1) + sizes.Select(size => Vector3.Project(size, absDirection)).Aggregate(ExtensionMethods.Add).magnitude;
+			float lengthSum = offsetFromEnd ?
+				-offset * (inventory.Count - 1) + sizes.Select(size => Vector3.Project(size, absDirection)).Aggregate(ExtensionMethods.Add).magnitude :
+				offset * (inventory.Count - 1);
 			
 			Vector3 startRight = -lengthSum / 2 * direction;
 			Vector3 right = startRight;
@@ -26,21 +31,23 @@ namespace SBEPIS.Capturllection.Deques
 				storable.Tick(deltaTime, Quaternion.Euler(0, 0, -60) * direction);
 				
 				float length = Vector3.Project(size, absDirection).magnitude;
-				right += direction * length / 2;
+				right += direction * (offsetFromEnd ? length / 2 : 0);
 				
 				Vector3 up = Mathf.Sin(state.time * wobbleTimeFactor + (right - startRight).magnitude * wobbleSpaceFactor) * wobbleAmplitude * Vector3.up;
 				
 				storable.position = right + up;
 				storable.rotation = Quaternion.identity;
 				
-				right += direction * (length / 2 - overlap);
+				right += direction * (offset + (offsetFromEnd ? length / 2 : 0));
 			}
 		}
 		public override Vector3 GetMaxPossibleSizeOf(List<Storable> inventory)
 		{
 			List<Vector3> sizes = inventory.Select(storable => storable.maxPossibleSize).ToList();
 			Vector3 maxSize = sizes.Aggregate(ExtensionMethods.Max);
-			Vector3 sumSize = sizes.Aggregate(ExtensionMethods.Add) - overlap * (inventory.Count - 1) * Vector3.one;
+			Vector3 sumSize = offsetFromEnd ?
+				-offset * (inventory.Count - 1) * Vector3.one + sizes.Aggregate(ExtensionMethods.Add) :
+				offset * (inventory.Count - 1) * Vector3.one;
 			return ExtensionMethods.Max(maxSize, sumSize);
 		}
 		
