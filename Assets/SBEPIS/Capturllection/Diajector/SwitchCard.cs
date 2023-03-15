ec824d8bda11547e335e37b951df28197a381849
Jady Wright <iamjwright159@gmail.com>
2023-03-14 21:36:23 -0400

    Add a card switch and add it to Stack's settings

---
+++ Assets/SBEPIS/Capturllection/Diajector/SwitchCard.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SBEPIS.Capturllection
+{
+	public class SwitchCard : MonoBehaviour
+	{
+		public Transform offPoint;
+		public Transform onPoint;
+		public CardTarget target;
+
+		public UnityEvent<bool> onSwitchValueChanged = new();
+
+		private bool _switchValue;
+		public bool SwitchValue
+		{
+			get => _switchValue;
+			set
+			{
+				_switchValue = value;
+				target.transform.SetPositionAndRotation(
+					(_switchValue ? onPoint : offPoint).position,
+					(_switchValue ? onPoint : offPoint).rotation);
+				onSwitchValueChanged.Invoke(_switchValue);
+			}
+		}
+
+		public void ClampNewPosition()
+		{
+			SwitchValue = (transform.position - onPoint.position).sqrMagnitude < (transform.position - offPoint.position).sqrMagnitude;
+		}
+	}
+}
