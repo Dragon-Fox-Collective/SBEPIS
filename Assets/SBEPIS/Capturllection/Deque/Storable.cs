f267480c043ce62e5e4fbd6695fecdafac8476e4
Jady Wright <iamjwright159@gmail.com>
2023-03-18 01:43:05 -0400

    Clean up some imports

--- Assets/SBEPIS/Capturllection/Deque/Storable.cs
+++ Assets/SBEPIS/Capturllection/Deque/Storable.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace SBEPIS.Capturllection
 {
--- Assets/SBEPIS/Capturllection/Deque/StorableGroup.cs
+++ Assets/SBEPIS/Capturllection/Deque/StorableGroup.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace SBEPIS.Capturllection
 {
