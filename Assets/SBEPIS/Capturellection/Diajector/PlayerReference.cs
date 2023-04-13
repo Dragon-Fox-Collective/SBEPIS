3c20fd33957149b2673bf472dac28994dc5a492d
Jady Wright <iamjwright159@gmail.com>
2023-04-13 19:31:00 -0400

    Give the player reference a better way to get components

--- Assets/SBEPIS/Capturellection/Diajector/PlayerReference.cs
+++ Assets/SBEPIS/Capturellection/Diajector/PlayerReference.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
 
 namespace SBEPIS.Capturellection
 {
 	public class PlayerReference : MonoBehaviour
 	{
-		public GameObject reference;
+		public Transform reference;
+
+		public T GetReferencedComponent<T>()
+		{
+			if (!reference.TryGetComponent(out T component))
+				throw new NullReferenceException($"{this} doesn't have a {typeof(T).Name}");
+			return component;
+		}
 	}
 }
