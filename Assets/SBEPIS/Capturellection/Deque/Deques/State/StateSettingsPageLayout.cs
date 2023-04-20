ba52e1dd5f29eb92e76868675d2aca951b3c06a6
Jady Wright <iamjwright159@gmail.com>
2023-04-20 16:14:37 -0400

    Add state deque that only captures/fetches when state is true

--- Assets/SBEPIS/Capturellection/Deque/Deques/State/StateSettingsPageLayout.cs
+++ Assets/SBEPIS/Capturellection/Deque/Deques/State/StateSettingsPageLayout.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace SBEPIS.Capturellection.Deques
 {
-	public class QueueSettingsPageLayout : DequeSettingsPageLayout<QueueDeque>
+	public class StateSettingsPageLayout : DequeSettingsPageLayout<StateDeque>
 	{
 		public SwitchCardAttacher offsetFromEndSwitch;
 		
