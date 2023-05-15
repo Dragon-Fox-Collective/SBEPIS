121d7a3e906da4273c919c83409db4561f3e086e
Jady Wright <iamjwright159@gmail.com>
2023-05-14 22:34:55 -0400

    Add hashmap deque initial definition and hashing functions

---
+++ Assets/SBEPIS/Capturellection/Deque/Deques/Hashmap/HashmapDeque.cs
@@ -0,0 +1,21 @@
+using Cysharp.Threading.Tasks;
+using SBEPIS.Bits;
+using SBEPIS.Capturellection.Storage;
+using SBEPIS.Items;
+using UnityEngine;
+
+namespace SBEPIS.Capturellection.Deques
+{
+	public class HashmapDeque : LaidOutDeque<LinearLayout, LinearState>
+	{
+		[SerializeField] private HashFunction hashFunction;
+		
+		public override async UniTask<DequeStoreResult> StoreItem(LinearState state, Capturellectable item)
+		{
+			int index = hashFunction.Hash(BitManager.instance.Bits.BitSetToCode(item.GetComponentInParent<Item>().Module.Bits.Bits), state.Inventory.Count);
+			Storable storable = state.Inventory[index];
+			StorableStoreResult res = await storable.StoreItem(item);
+			return res.ToDequeResult(index, storable);
+		}
+	}
+}
