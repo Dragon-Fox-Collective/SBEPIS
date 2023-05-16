df4f8e78161d1e2cec9bcbe944ece4a653bdfc06
Jady Wright <iamjwright159@gmail.com>
2023-05-16 13:00:40 -0400

    Add querty layout keyboard

--- Assets/SBEPIS/Capturellection/Deque/Deques/Hashmap/Keyboard.cs
+++ Assets/SBEPIS/Capturellection/Deque/Deques/Hashmap/Keyboard.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using KBCore.Refs;
 using TMPro;
 using UnityEngine;
@@ -10,6 +12,10 @@ namespace SBEPIS.Capturellection.Deques
 		[SerializeField, Child] private TMP_Text textDisplay;
 		[SerializeField, Child] private Key[] keys;
 		[SerializeField] private string noTextDisplayString = "[name]";
+		
+		[TextArea]
+		[SerializeField] private string keyboardLayout = "QWERTYUIOP\nASDFGHJKL\n_ZXCVBNM←";
+		[SerializeField] private Vector2 spacing = new(0.1f, 0.15f);
 		
 		public UnityEvent<string> onType = new();
 		
@@ -28,8 +34,21 @@ namespace SBEPIS.Capturellection.Deques
 		private void Awake()
 		{
 			Text = "";
+			List<List<string>> positions = keyboardLayout.Split('\n').Select(line => line.Select(ch => ch.ToString()).ToList()).ToList();
 			foreach (Key key in keys)
+			{
+				int lineIndex = positions.FindIndex(line => line.Contains(key.DisplayText));
+				List<string> line = positions[lineIndex];
+				int chIndex = line.IndexOf(key.DisplayText);
+				
+				key.transform.localPosition = new Vector3(
+					(chIndex - (line.Count - 1) / 2f) * spacing.x,
+					(lineIndex - (positions.Count - 1) / 2f) * -spacing.y,
+					0
+				);
+				
 				key.CardTarget.onGrab.AddListener(() => Text = key.Handle(Text));
+			}
 		}
 	}
 }
