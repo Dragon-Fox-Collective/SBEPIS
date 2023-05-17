9381968aa56bbd8b2c61fc4f6b35f09abb224f38
Jady Wright <iamjwright159@gmail.com>
2023-05-17 16:01:26 -0400

    Prevent cards from being fetched twice before fetching

--- Assets/SBEPIS/Capturellection/Card/Capturellector.cs
+++ Assets/SBEPIS/Capturellection/Card/Capturellector.cs
@@ -27,12 +27,15 @@ namespace SBEPIS.Capturellection
 			
 			if (grabber.HeldGrabbable.TryGetComponent(out CaptureContainer container))
 				RetrieveAndGrabItem(container).Forget();
-			else if (grabber.HeldGrabbable.TryGetComponent(out Capturellectable item) && !item.IsBeingCaptured)
+			else if (grabber.HeldGrabbable.TryGetComponent(out Capturellectable item))
 				CaptureAndGrabCard(item).Forget();
 		}
 		
 		public async UniTask<CaptureContainer> CaptureAndGrabCard(Capturellectable item)
 		{
+			if (item.IsBeingCaptured)
+				return null;
+			
 			StorableStoreResult result = await inventory.StoreItem(item);
 			MoveEjectedItem(result.card, result.ejectedItem);
 			TryGrab(result.card.transform).Forget();
@@ -65,7 +68,7 @@ namespace SBEPIS.Capturellection
 		
 		private async UniTask<Capturellectable> RetrieveFromInventory(InventoryStorable card)
 		{
-			if (!inventory.CanFetch(card))
+			if (!inventory.CanFetch(card) || card.IsBeingFetched)
 				return null;
 			
 			Capturellectable item = await inventory.FetchItem(card);
