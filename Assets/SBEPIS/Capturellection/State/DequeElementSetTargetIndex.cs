f7cc1fc7e7947077a1992ff538e569ebff7a8ef4
Jady Wright <iamjwright159@gmail.com>
2023-06-08 19:51:02 -0400

    Fix backward target targetting wrong target, make settarget teleporting optional

--- Assets/SBEPIS/Capturellection/State/DequeElementSetTargetIndex.cs
+++ Assets/SBEPIS/Capturellection/State/DequeElementSetTargetIndex.cs
@@ -10,22 +10,31 @@ namespace SBEPIS.Capturellection.State
 		[SerializeField] private FlexibleDequeElement dequeElement = new();
 		[Tooltip("Negative values count from the end of the list")]
 		[SerializeField] private FlexibleInt index = new();
+		[SerializeField] private FlexibleBool teleport = new();
 		[SerializeField] private IntParameterReference currentTargetIndex = new();
 		
 		public override void OnStateBegin()
 		{
-			int finalTargetIndex = index.value >= 0 ? index.value : dequeElement.value.Page.Diajector.LerpTargetCount + index.value - 1;
+			int finalTargetIndex = index.value >= 0 ? index.value : dequeElement.value.Page.Diajector.LerpTargetPathCount + index.value;
+			if (finalTargetIndex == currentTargetIndex.value)
+				return;
+			
+			Vector3 oldPosition = dequeElement.value.transform.position;
+			Quaternion oldRotation = dequeElement.value.transform.rotation;
+			
 			if (finalTargetIndex > currentTargetIndex.value)
 				CountUp(dequeElement.value, finalTargetIndex);
 			else if (finalTargetIndex < currentTargetIndex.value)
 				CountDown(dequeElement.value, finalTargetIndex);
+			
+			if (!teleport.value)
+				dequeElement.value.transform.SetPositionAndRotation(oldPosition, oldRotation);
 		}
 		
 		private void CountUp(DequeElement dequeElement, int finalTargetIndex)
 		{
 			for (; currentTargetIndex.value < finalTargetIndex; currentTargetIndex.value++)
 				dequeElement.TeleportToLerpTarget(currentTargetIndex.value + 1);
-			dequeElement.TeleportToLerpTarget(currentTargetIndex.value + 1);
 		}
 		
 		private void CountDown(DequeElement dequeElement, int finalTargetIndex)
