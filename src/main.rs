848e74864181f9cd8b1bbe0385e2123c5cbaa858
Jade Wright <iamjwright159@gmail.com>
2023-10-04 14:31:58 -0400

    Add planet generating function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::*;
 
-fn main() {
+fn main()
+{
 	App::new()
 		.add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+		.insert_resource(Gravity(Vec3::ZERO))
 		.add_systems(Startup, setup)
 		.run();
 }
+
+#[derive(Component)]
+pub struct GravitationalField {
+	gravity: f64
+}
+
+#[derive(Component)]
+pub struct AffectedByGravity { }
 
 fn setup(
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-	// Plane
-	commands.spawn((
-		PbrBundle {
-			mesh: meshes.add(Mesh::from(shape::Plane::from_size(8.0))),
-			material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-			..default()
-		},
-		RigidBody::Static,
-		Collider::cuboid(8.0, 0.002, 8.0),
-	));
+	spawn_planet(Vec3::new(0.0, -6.0, 0.0), 6.0, &mut commands, &mut meshes, &mut materials);
+	
 	// Cube
 	commands.spawn((
 		PbrBundle {
@@ -35,6 +37,7 @@ fn setup(
 		AngularVelocity(Vec3::new(2.5, 3.4, 1.6)),
 		Collider::cuboid(1.0, 1.0, 1.0),
 	));
+
 	// Light
 	commands.spawn(PointLightBundle {
 		point_light: PointLight {
@@ -45,9 +48,30 @@ fn setup(
 		transform: Transform::from_xyz(4.0, 8.0, 4.0),
 		..default()
 	});
+	
 	// Camera
 	commands.spawn(Camera3dBundle {
 		transform: Transform::from_xyz(-4.0, 6.5, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
 		..default()
 	});
+}
+
+fn spawn_planet
+(
+	position: Vec3,
+	radius: f32,
+	commands: &mut Commands,
+	meshes: &mut Assets<Mesh>,
+	materials: &mut Assets<StandardMaterial>,
+) {
+	commands.spawn((
+		PbrBundle {
+			mesh: meshes.add(Mesh::from(shape::UVSphere { radius, sectors: 16, stacks: 16 })),
+			material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+			..default()
+		},
+		RigidBody::Static,
+		Collider::ball(radius),
+		Position(position),
+	));
 }
