8d108e16e0960c894deea74a2232b965c9931407
Jade Wright <iamjwright159@gmail.com>
2023-10-11 16:54:30 -0400

    Move player commands and notes and editor file into modules

--- src/player_commands/mod.rs
+++ src/player_commands/mod.rs
@@ -1,7 +1,12 @@
+mod notes;
+
 use bevy::prelude::*;
 use bevy_input::common_conditions::input_just_pressed;
 use bevy_audio::PlaybackMode;
+
 use super::util::MapRange;
+
+use self::notes::*;
 
 pub struct PlayerCommandsPlugin;
 
@@ -176,140 +181,6 @@ fn close_staff(
 {
 	staff.is_open = false;
 	style.display = Display::None;
-}
-
-#[derive(Debug)]
-#[allow(dead_code)]
-enum Note
-{
-	C0, CS0, D0, DS0, E0, F0, FS0, G0, GS0, A0, AS0, B0,
-	C1, CS1, D1, DS1, E1, F1, FS1, G1, GS1, A1, AS1, B1,
-	C2, CS2, D2, DS2, E2, F2, FS2, G2, GS2, A2, AS2, B2,
-	C3, CS3, D3, DS3, E3, F3, FS3, G3, GS3, A3, AS3, B3,
-	C4, CS4, D4, DS4, E4, F4, FS4, G4, GS4, A4, AS4, B4,
-	C5, CS5, D5, DS5, E5, F5, FS5, G5, GS5, A5, AS5, B5,
-	C6, CS6, D6, DS6, E6, F6, FS6, G6, GS6, A6, AS6, B6,
-	C7, CS7, D7, DS7, E7, F7, FS7, G7, GS7, A7, AS7, B7,
-	C8, CS8, D8, DS8, E8, F8, FS8, G8, GS8, A8, AS8, B8,
-}
-
-impl Note
-{
-	/// In Hz
-	fn frequency(&self) -> f32
-	{
-		match self
-		{
-			Note::C0 => 16.35,
-			Note::CS0 => 17.32,
-			Note::D0 => 18.35,
-			Note::DS0 => 19.45,
-			Note::E0 => 20.60,
-			Note::F0 => 21.83,
-			Note::FS0 => 23.12,
-			Note::G0 => 24.50,
-			Note::GS0 => 25.96,
-			Note::A0 => 27.50,
-			Note::AS0 => 29.14,
-			Note::B0 => 30.87,
-			Note::C1 => 32.70,
-			Note::CS1 => 34.65,
-			Note::D1 => 36.71,
-			Note::DS1 => 38.89,
-			Note::E1 => 41.20,
-			Note::F1 => 43.65,
-			Note::FS1 => 46.25,
-			Note::G1 => 49.00,
-			Note::GS1 => 51.91,
-			Note::A1 => 55.00,
-			Note::AS1 => 58.27,
-			Note::B1 => 61.74,
-			Note::C2 => 65.41,
-			Note::CS2 => 69.30,
-			Note::D2 => 73.42,
-			Note::DS2 => 77.78,
-			Note::E2 => 82.41,
-			Note::F2 => 87.31,
-			Note::FS2 => 92.50,
-			Note::G2 => 98.00,
-			Note::GS2 => 103.83,
-			Note::A2 => 110.00,
-			Note::AS2 => 116.54,
-			Note::B2 => 123.47,
-			Note::C3 => 130.81,
-			Note::CS3 => 138.59,
-			Note::D3 => 146.83,
-			Note::DS3 => 155.56,
-			Note::E3 => 164.81,
-			Note::F3 => 174.61,
-			Note::FS3 => 185.00,
-			Note::G3 => 196.00,
-			Note::GS3 => 207.65,
-			Note::A3 => 220.00,
-			Note::AS3 => 233.08,
-			Note::B3 => 246.94,
-			Note::C4 => 261.63,
-			Note::CS4 => 277.18,
-			Note::D4 => 293.66,
-			Note::DS4 => 311.13,
-			Note::E4 => 329.63,
-			Note::F4 => 349.23,
-			Note::FS4 => 369.99,
-			Note::G4 => 392.00,
-			Note::GS4 => 415.30,
-			Note::A4 => 440.00,
-			Note::AS4 => 466.16,
-			Note::B4 => 493.88,
-			Note::C5 => 523.25,
-			Note::CS5 => 554.37,
-			Note::D5 => 587.33,
-			Note::DS5 => 622.25,
-			Note::E5 => 659.25,
-			Note::F5 => 698.46,
-			Note::FS5 => 739.99,
-			Note::G5 => 783.99,
-			Note::GS5 => 830.61,
-			Note::A5 => 880.00,
-			Note::AS5 => 932.33,
-			Note::B5 => 987.77,
-			Note::C6 => 1046.50,
-			Note::CS6 => 1108.73,
-			Note::D6 => 1174.66,
-			Note::DS6 => 1244.51,
-			Note::E6 => 1318.51,
-			Note::F6 => 1396.91,
-			Note::FS6 => 1479.98,
-			Note::G6 => 1567.98,
-			Note::GS6 => 1661.22,
-			Note::A6 => 1760.00,
-			Note::AS6 => 1864.66,
-			Note::B6 => 1975.53,
-			Note::C7 => 2093.00,
-			Note::CS7 => 2217.46,
-			Note::D7 => 2349.83,
-			Note::DS7 => 2489.02,
-			Note::E7 => 2637.02,
-			Note::F7 => 2793.83,
-			Note::FS7 => 2959.96,
-			Note::G7 => 3135.96,
-			Note::GS7 => 3322.44,
-			Note::A7 => 3520.00,
-			Note::AS7 => 3729.31,
-			Note::B7 => 3951.07,
-			Note::C8 => 4186.01,
-			Note::CS8 => 4434.92,
-			Note::D8 => 4698.63,
-			Note::DS8 => 4978.03,
-			Note::E8 => 5274.04,
-			Note::F8 => 5587.65,
-			Note::FS8 => 5919.91,
-			Note::G8 => 6271.93,
-			Note::GS8 => 6644.88,
-			Note::A8 => 7040.00,
-			Note::AS8 => 7458.62,
-			Note::B8 => 7902.13,
-		}
-	}
 }
 
 fn play_notes(
