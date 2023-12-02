d151be9ed2f948d8b4b63691c59889cbcdb1ede3
Jade Wright <iamjwright159@gmail.com>
2023-12-02 14:36:30 -0500

    Add an item sensor
    Closes #125

---
+++ Assets/SBEPIS/Items/ItemSensor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SBEPIS.Items
+{
+	public class ItemSensor : MonoBehaviour
+	{
+		public UnityEvent<Item> onItemChanged = new();
+		
+		private Item item;
+		public Item Item
+		{
+			get => item;
+			set
+			{
+				if (item == value)
+					return;
+				
+				item = value;
+				onItemChanged.Invoke(item);
+			}
+		}
+		
+		private void OnTriggerEnter(Collider other)
+		{
+			if (Item != null) return;
+			if (!other.attachedRigidbody) return;
+			if (!other.attachedRigidbody.TryGetComponent(out Item collisionItem)) return;
+			
+			Item = collisionItem;
+		}
+		
+		private void OnTriggerExit(Collider other)
+		{
+			if (Item == null) return;
+			if (!other.attachedRigidbody) return;
+			if (!other.attachedRigidbody.TryGetComponent(out Item collisionItem)) return;
+			
+			if (Item == collisionItem)
+				Item = null;
+		}
+	}
+}
