3f51f5f3b61ece31a67ab0cf3a7c4057506cd907
Jady Wright <iamjwright159@gmail.com>
2023-12-16 11:13:18 -0500

    Add crouching
    Closes #137

---
+++ Assets/SBEPIS/Controller/CrouchController.cs
@@ -0,0 +1,38 @@
+﻿using KBCore.Refs;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace SBEPIS.Controller
+{
+	public class CrouchController : ValidatedMonoBehaviour
+	{
+		[SerializeField, Self] private ConfigurableJoint joint;
+		[SerializeField, Anywhere] private Transform headTracker;
+		[SerializeField] private float crouchDistance = 0.5f;
+		
+		private Vector3 offset => Crouching ? Vector3.down * crouchDistance : Vector3.zero;
+
+		private bool crouching;
+		public bool Crouching
+		{
+			get => crouching;
+			set
+			{
+				if (value == crouching) return;
+				crouching = value;
+				joint.connectedAnchor = startingAnchor + offset;
+				headTracker.localPosition = headTrackerStartingPosition + offset;
+			}
+		}
+		
+		private Vector3 startingAnchor;
+		private Vector3 headTrackerStartingPosition;
+		
+		private void Start()
+		{
+			startingAnchor = joint.connectedAnchor;
+			joint.autoConfigureConnectedAnchor = false;
+			headTrackerStartingPosition = headTracker.localPosition;
+		}
+	}
+}
