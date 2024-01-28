346513e7ca305f8bf47562b751297767b0929017
Jady Wright <iamjwright159@gmail.com>
2024-01-27 21:29:58 -0500

    Make the jet apply force

---
+++ Assets/SBEPIS/Items/Modules/Jet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SBEPIS.Items.Modules
+{
+	public class Jet : MonoBehaviour
+	{
+		[SerializeField] private float force = 50;
+		
+		private new Rigidbody rigidbody;
+		
+		private void Start()
+		{
+			rigidbody = GetComponentInParent<Rigidbody>();
+		}
+		
+		private void FixedUpdate()
+		{
+			if (rigidbody)
+				rigidbody.AddForceAtPosition(-transform.up * force, transform.position);
+		}
+	}
+}
