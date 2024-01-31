23c8b7f8aef16cf9172174cbda502619eb4bff70
Jady Wright <iamjwright159@gmail.com>
2024-01-31 11:41:01 -0500

    Reverse deque deck load order
    Fix #163

--- Assets/SBEPIS/Utils/Counter.cs
+++ Assets/SBEPIS/Utils/Counter.cs
@@ -6,23 +6,22 @@ namespace SBEPIS.Utils
 {
 	public class Counter : MonoBehaviour
 	{
+		[SerializeField] private int count = 0;
+		public int Count
+		{
+			get => count;
+			private set
+			{
+				count = Mathf.Clamp(value, useMin ? min : int.MinValue, useMax ? max : int.MaxValue);
+				onCountChanged.Invoke(count);
+			}
+		}
 		[SerializeField] private bool useMin;
 		[SerializeField] private int min;
 		[SerializeField] private bool useMax;
 		[SerializeField] private int max;
 		
 		public UnityEvent<int> onCountChanged = new();
-		
-		private int count = 0;
-		public int Count
-		{
-			get => count;
-			private set
-			{
-				count = Mathf.Clamp(value, useMin ? min : Int32.MinValue, useMax ? max : Int32.MaxValue);
-				onCountChanged.Invoke(count);
-			}
-		}
 		
 		public void Increment() => Count++;
 		public void Decrement() => Count--;
