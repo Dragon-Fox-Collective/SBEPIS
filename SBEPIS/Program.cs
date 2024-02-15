6a79e5f6c9db7eddc48e373341169874202e5f68
Jady Wright <iamjwright159@gmail.com>
2024-02-15 14:25:36 -0500

    Finish importing Echinda

--- SBEPIS/Program.cs
+++ SBEPIS/Program.cs
@@ -1,5 +1,26 @@
-﻿using Echidna2.Mathematics;
+﻿using Echidna2.Core;
+using Echidna2.Rendering;
+using OpenTK.Windowing.Desktop;
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(Vector2.One * 4);
+
+Hierarchy root = new();
+
+
+IHasChildren.PrintTree(root);
+
+Window window = new(new GameWindow(
+	new GameWindowSettings(),
+	new NativeWindowSettings
+	{
+		ClientSize = (1280, 720),
+		Title = "SBEPIS",
+	}
+))
+{
+	Camera = new Camera { World = root }
+};
+// window.Resize += size => root.LocalSize = size;
+window.Run();
+return;
