d6b9640b8f9adddf8d0078417852bc9dc4ce3a00
Jady Wright <iamjwright159@gmail.com>
2024-02-26 16:53:10 -0500

    Serialize the consorts

--- SBEPIS/Program.cs
+++ SBEPIS/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Drawing;
-using Echidna2.Core;
-using Echidna2.Mathematics;
+﻿using Echidna2.Core;
 using Echidna2.Physics;
 using Echidna2.Rendering;
 using Echidna2.Serialization;
@@ -16,40 +14,6 @@ Console.WriteLine("Hello, World!");
 Scene root = TomlSerializer.Deserialize<Scene>($"{AppContext.BaseDirectory}/Prefabs/Scene.toml");
 
 
-
-AddConsort((0, 0, 0), (255, 255, 255), ao: 300.0);
-
-AddConsort((-10, 0, 0), (0, 255, 255));
-AddConsort((+10, 0, 0), (255, 0, 0));
-AddConsort((0, -10, 0), (255, 0, 255));
-AddConsort((0, +10, 0), (0, 255, 0));
-AddConsort((0, 0, -10), (255, 255, 0));
-AddConsort((0, 0, +10), (0, 0, 255));
-
-AddConsort((0, -10, 10), (127, 0, 255));
-AddConsort((0, -10, -10), (255, 127, 0));
-AddConsort((0, 10, -10), (127, 255, 0));
-AddConsort((0, 10, 10), (0, 127, 127));
-AddConsort((10, 0, 10), (127, 0, 127));
-AddConsort((-10, 0, -10), (127, 255, 127));
-AddConsort((-10, 0, 10), (0, 127, 255));
-AddConsort((10, 0,  -10), (255, 127, 0));
-AddConsort((10, 10, 0), (127, 127, 0));
-AddConsort((-10, 10, 0), (0, 255, 127));
-AddConsort((10, -10, 0), (255, 0, 127));
-AddConsort((-10, -10, 0), (127, 127, 255));
-
-AddConsort((10, 10, 10), (63, 63, 63));
-AddConsort((-10, 10, 10), (0, 191, 191));
-AddConsort((10, -10, 10), (191, 0, 191));
-AddConsort((-10, -10, 10), (63, 63, 191));
-AddConsort((10, 10, -10), (191, 191, 0));
-AddConsort((-10, 10, -10), (63, 191, 63));
-AddConsort((10, -10, -10), (191, 63, 63));
-AddConsort((-10, -10, -10), (191, 191, 191));
-
-
-//IHasChildren.PrintTree(root);
 
 WorldSimulation simulation = new(root);
 root.AddChild(simulation);
@@ -88,15 +52,3 @@ window.Resize += size =>
 	root.Player.Camera.Size = size;
 };
 window.Run();
-return;
-
-void AddConsort(Vector3 position, Vector3 color, double ao = 1.0)
-{
-	// position += new Vector3(Random.Shared.NextDouble(), Random.Shared.NextDouble(), Random.Shared.NextDouble());
-	position += Vector3.Up * 5;
-	Consort consort = TomlSerializer.Deserialize<Consort>($"{AppContext.BaseDirectory}/Prefabs/Consort.toml");
-	consort.LocalPosition = position;
-	consort.MeshRenderer.Albedo = Color.FromArgb((int)color.X, (int)color.Y, (int)color.Z);
-	consort.MeshRenderer.AmbientOcclusion = ao;
-	root.AddChild(consort);
-}
